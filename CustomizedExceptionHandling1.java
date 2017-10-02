997095fcaa64bdc4ef9f5a2d837ab1e36c79ccb6
PrabhudeepSingh <prabhudeepsb@gmail.com>
2017-10-02 22:58:02 +0530

    To illustrate the concept of Customized Exception Handling1 in java.

---
+++ CustomizedExceptionHandling1.java
@@ -0,0 +1,19 @@
+public class CustomizedExceptionHandling1
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Start");
+	
+    try
+    {  
+      System.out.println(10/0);
+    }
+	
+    catch(ArithmeticException e)
+    {
+      System.out.println(10/2);
+    }
+    
+	System.out.println("End");
+  }
+}
